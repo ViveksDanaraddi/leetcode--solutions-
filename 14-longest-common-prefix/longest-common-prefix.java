1c83f63169fde1e22af6184fb4879f5a76cb0a98
ViveksDanaraddi <vivekdanaraddi95@gmail.com>
2026-09-04 13:42:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (50.53%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            while (!strs[i].startsWith(prefix)) {
+
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"flower", "flow", "flight"};
+
+        Solution sol = new Solution();
+
+        String result = sol.longestCommonPrefix(strs);
+
+        System.out.println(result);
+    }
+}
